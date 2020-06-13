3464489875f8fffbe506e565b4522d7f312b635a
rafiswe <rafikulislam521@gmail.com>
2020-06-14 00:35:53 +0600

    bootstrap, router installation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Navbar, Nav, NavDropdown} from 'react-bootstrap';
+import {Switch, Route, Link} from "react-router-dom";
+import Home from './components/Home';
+import About from './components/About';
+import Policy from './components/Policy';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	<div className="App">
+		<Navbar bg="light" expand="lg">
+			<Navbar.Brand to="/">Home</Navbar.Brand>
+			<Navbar.Toggle aria-controls="basic-navbar-nav" />
+			<Navbar.Collapse id="basic-navbar-nav">
+			<Nav className="mr-auto">
+				<Nav.Link href="/">Home</Nav.Link>
+				<Nav.Link href="/about">About Us</Nav.Link>
+				<Nav.Link href="/policy">Policy</Nav.Link>
+				<NavDropdown title="Services" id="basic-nav-dropdown">
+				<NavDropdown.Item href="/">Action</NavDropdown.Item>
+				<NavDropdown.Item href="/">Another action</NavDropdown.Item>
+				<NavDropdown.Item href="/">Something</NavDropdown.Item>
+				</NavDropdown>
+			</Nav>
+			</Navbar.Collapse>
+		</Navbar>
+		<Switch>
+			<Route path="/" exact component={Home} />
+			<Route path="/about" component={About} />
+			<Route path="/policy" component={Policy} />
+		</Switch>
+	</div>
   );
 }
 
---
+++ src/components/Policy.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Policy() {
+  return (
+    <div>
+      <p>welcome to Policy</p>
+    </div>
+  );
+}
